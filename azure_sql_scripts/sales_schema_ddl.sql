abc5aa0bc4119211a23fd2573358580aca007de2
saurabhchavan7 <cvnsaurabh7@gmail.com>
2025-01-18 19:37:28 -0500

    sql script added

---
+++ azure_sql_scripts/sales_schema_ddl.sql
@@ -0,0 +1,15 @@
+CREATE TABLE source_salesdata
+(
+	Branch_ID Varchar(200),
+	Dealer_ID Varchar(200),
+	Model_ID Varchar(200),
+	Revenue BIGINT,
+	Units_sold BIGINT,
+	Date_ID Varchar(200),
+	Day INT,
+	Month INT,
+	Year INT,
+	BranchName Varchar(2000),
+	DealerName Varchar(2000),
+	Product_Name Varchar(2000),
+	Country Varchar(2000)
